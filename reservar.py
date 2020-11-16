fc65d4916735e4f1c31eb394af35cc7605c3b4fe
Ruben <rubenfb14@gmail.com>
2020-11-16 22:26:29 +0100

    18

--- reservar.py
+++ reservar.py
@@ -29,6 +29,6 @@ pasw.send_keys(PASS)
 driver.find_element_by_name("commit").click()
 driver.get("https://crosshero.com/dashboard/classes?date=" + str(tomorrow.day) + "%2F" +  str(tomorrow.month) + "%2F" +  str(tomorrow.year) + "&program_id=599ea218204351000ec9130d")
 driver.current_url
-reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '15:00')]")
+reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '18:00')]")
 reservation_hour[0].click()
 driver.find_element_by_id("classes-sign-in").click()
