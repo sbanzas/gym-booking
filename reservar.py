70657b61d6eb733fd0ca765a2158b5cca63845ee
Ruben <rubenfb14@gmail.com>
2020-11-17 15:27:44 +0100

    add comments to use locally

--- reservar.py
+++ reservar.py
@@ -14,12 +14,15 @@ LOCAL_CHROMEDRIVER_PATH = './chromedriver'
 
 chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
 options = webdriver.ChromeOptions()
-#options.binary_location = chrome_bin
+if chrome_bin:
+    options.binary_location = chrome_bin
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+#Comment this if you want to see the browser (only locally)
 options.add_argument('headless')
 options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path=LOCAL_CHROMEDRIVER_PATH, options=options)
+#Use LOCAL_CHROMEDRIVER_PATH here if you execute it locally 
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
@@ -31,7 +34,7 @@ pasw = driver.find_element_by_id("athlete_password")
 pasw.send_keys(PASS)
 driver.find_element_by_name("commit").click()
 driver.get("https://crosshero.com/dashboard/classes?date=" + str(tomorrow.day) + "%2F" +  str(tomorrow.month) + "%2F" +  str(tomorrow.year) + "&program_id=599ea218204351000ec9130d")
-reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '15:00')]")
+reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '19:30')]")
 reservation_hour[0].click()
 try:
     driver.find_element_by_id("classes-sign-in").click()
