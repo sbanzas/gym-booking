0f6a7fd5eff6af8db308c5af8d07648e39a39e38
Ruben <rubenfb14@gmail.com>
2020-11-17 01:26:25 +0100

    use session string

--- telebot.py
+++ telebot.py
@@ -6,14 +6,14 @@ from telethon import TelegramClient, sync, events
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-
-
 api_id = '***REMOVED***'
 api_hash = '***REMOVED***'
 token = '***REMOVED***'
 
 phone = '***REMOVED***'
-session_string = "***REMOVED***"
+ruben_session_string = """***REMOVED***"""
+
+bot_session_string = """***REMOVED***"""
 
 def main():
 	with TelegramClient(StringSession(), api_id, api_hash) as client:
@@ -25,12 +25,12 @@ if __name__ == "__main__":
 	
 
 def sendfile(filepath):
-	client = TelegramClient(session_string, api_id, api_hash) 
-	try: 
-		receiver1 = client.get_entity('@benru89')
-		receiver2 = client.get_entity('@Acadesebi')
-		client.send_file(receiver1, filepath) 
-		client.send_message(receiver2, "Entretenme payaso y apuntate", parse_mode='html') 
-		
-	except Exception as e: 
-		print(e); 
+	client = TelegramClient(StringSession(bot_session_string), api_id, api_hash) 
+	client.connect() 
+
+	receiver1 = client.get_entity('@benru89')
+	receiver2 = client.get_entity('@Acadesebi')
+	client.send_file(receiver1, filepath) 
+	#client.send_message(receiver2, "Entretenme payaso y apuntate", parse_mode='html') 
+			 
+	client.disconnect()
