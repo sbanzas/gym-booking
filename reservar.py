c6d63b25683f773b776fecf0eafb8848bf5a9522
Ruben <rubenfb14@gmail.com>
2020-11-17 13:48:35 +0100

    dont wait, just click right away and if not refresh the page and click again

--- reservar.py
+++ reservar.py
@@ -14,33 +14,33 @@ LOCAL_CHROMEDRIVER_PATH = './chromedriver'
 
 chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
 options = webdriver.ChromeOptions()
-options.binary_location = chrome_bin
+#options.binary_location = chrome_bin
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument('headless')
 options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
+driver = webdriver.Chrome(executable_path=LOCAL_CHROMEDRIVER_PATH, options=options)
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 
 driver.get("https://crosshero.com/athletes/sign_in")
-driver.current_url
 user = driver.find_element_by_id("athlete_email") 
 user.send_keys(USER)
 pasw = driver.find_element_by_id("athlete_password")
 pasw.send_keys(PASS)
 driver.find_element_by_name("commit").click()
 driver.get("https://crosshero.com/dashboard/classes?date=" + str(tomorrow.day) + "%2F" +  str(tomorrow.month) + "%2F" +  str(tomorrow.year) + "&program_id=599ea218204351000ec9130d")
-reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '13:30')]")
+reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '15:00')]")
 reservation_hour[0].click()
 try:
-    element = WebDriverWait(driver, 100).until(
-        EC.presence_of_element_located((By.ID, "classes-sign-in"))
-    )
-    element.click()
-except:
-    print("no quedan sitios")
+    driver.find_element_by_id("classes-sign-in").click()
+except Exception as e:
+    driver.refresh()
+    try:
+    	driver.find_element_by_id("classes-sign-in").click()
+    except:
+    	print("non quedan sitios")
     
 time.sleep(10) 
 driver.save_screenshot('screenie.png')
