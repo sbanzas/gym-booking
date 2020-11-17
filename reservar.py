9d1236c323a5c36163cbf68d88f55d8d88129856
Ruben <rubenfb14@gmail.com>
2020-11-17 01:34:53 +0100

    wait until button appears

--- reservar.py
+++ reservar.py
@@ -1,5 +1,8 @@
 import datetime
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os, time
 import telebot as bot
 
@@ -11,12 +14,12 @@ LOCAL_CHROMEDRIVER_PATH = './chromedriver'
 
 chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
 options = webdriver.ChromeOptions()
-#options.binary_location = chrome_bin
+options.binary_location = chrome_bin
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument('headless')
 options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path=LOCAL_CHROMEDRIVER_PATH, options=options)
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
@@ -29,14 +32,16 @@ pasw = driver.find_element_by_id("athlete_password")
 pasw.send_keys(PASS)
 driver.find_element_by_name("commit").click()
 driver.get("https://crosshero.com/dashboard/classes?date=" + str(tomorrow.day) + "%2F" +  str(tomorrow.month) + "%2F" +  str(tomorrow.year) + "&program_id=599ea218204351000ec9130d")
-driver.current_url
 reservation_hour = driver.find_elements_by_xpath("//*[contains(text(), '19:30')]")
 reservation_hour[0].click()
 try:
-	driver.find_element_by_id("classes-sign-in").click()
-except:
-	print("no quedan sitios")
+    element = WebDriverWait(driver, 100).until(
+        EC.presence_of_element_located((By.ID, "classes-sign-in"))
+    )
+finally:
+    driver.quit()
+    print("no quedan sitios")
+
 time.sleep(11) 
 driver.save_screenshot('screenie.png')
 bot.sendfile("./screenie.png")
-
